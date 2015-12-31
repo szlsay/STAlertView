fe41656858faae066ef107be8dc290b15a1fe1b7
shenzhaoliang <409178030@qq.com>
2015-12-31 14:48:33 +0800

    2015-12-31 14:48:11 创建文件

---
+++ STAlertViewDemo/STAlertViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  STAlertViewDemo
+//
+//  Created by https://github.com/STShenZhaoliang/STAlertView on 15/12/31.
+//  Copyright © 2015年 ST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ STAlertViewDemo/STAlertViewDemo/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  STAlertViewDemo
+//
+//  Created by https://github.com/STShenZhaoliang/STAlertView on 15/12/31.
+//  Copyright © 2015年 ST. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+#pragma mark - lift cycle 生命周期
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+#pragma mark - Delegate 视图委托
+
+#pragma mark - event response 事件相应
+
+#pragma mark - private methods 私有方法
+
+#pragma mark - getters and setters 属性
+
+@end
